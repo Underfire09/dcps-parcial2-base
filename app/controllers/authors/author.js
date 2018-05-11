50954ddfc59a7f43c193a9766b14df14868e558d
Underfire09 <31070651+Underfire09@users.noreply.github.com>
2018-05-11 12:26:16 -0500

    Parcial2Final

--- app/controllers/authors/author.js
+++ app/controllers/authors/author.js
@@ -2,8 +2,8 @@ import Controller from '@ember/controller';
 import { computed } from "@ember/object";
 
 export default Controller.extend({
-    PostDelete: computed('model.post.@each.isDeleted', function () {
-		return this.get('model.post').filterBy('isDeleted', false);
+    PostNoDelete: computed('model.posts.@each.isDeleted', function () {
+		return this.get('model.posts').filterBy('isDeleted', false);
     }),
     actions:{
         delete(post){
